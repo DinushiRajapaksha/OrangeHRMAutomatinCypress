4c3cbae02118b2bbf46eb3d7ae3126c311c69ba1
dinushi.r <dinushi.r@creativesoftware.com>
2023-08-16 23:42:25 +0530

    initial commit

---
+++ cypress/e2e/demo.cy.js
@@ -0,0 +1,42 @@
+/// <reference types="cypress" />
+
+
+
+it.only('google test', function() {
+
+  cy.visit('https://google.com')//to visit google website
+
+  cy.get('#APjFqb').type('creative software {enter}')//to search and enter
+
+})
+
+
+it('google test', function() { //.only dmmama aka withri run wenne 
+
+  cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+
+  cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+
+  cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+
+  cy.get('.oxd-button').click()
+
+
+  cy.get(':nth-child(1) > .oxd-main-menu-item > .oxd-text').click()
+
+  cy.get('.--visited > .oxd-topbar-body-nav-tab-item').click()
+
+  cy.get('.oxd-dropdown-menu > li').click()
+
+  cy.get('.orangehrm-header-container > .oxd-button').click()
+  
+  cy.get(':nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-select-wrapper > .oxd-select-text > .oxd-select-text--after > .oxd-icon').click()
+
+ // cy.get('#')
+
+
+
+
+
+})
+
